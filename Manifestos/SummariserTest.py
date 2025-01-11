50bf810274acb709c53e552b3a8ae6d9efe2e599
Tuma, Max <mt2023@hw.ac.uk>
2025-01-11 22:23:25 +0000

    added labour manifesto summary

--- Manifestos/SummariserTest.py
+++ Manifestos/SummariserTest.py
@@ -1,14 +1,20 @@
 import torch
 from transformers import AutoTokenizer, AutoModelWithLMHead
+import os
 
 tokenizer = AutoTokenizer.from_pretrained('t5-base')
 model = AutoModelWithLMHead.from_pretrained('t5-base', return_dict=True)
 
-sequence = "Data science is an interdisciplinary field[10] focused on extracting knowledge from typically large data sets and applying the knowledge and insights from that data to solve problems in a wide range of application domains.[11] The field encompasses preparing data for analysis, formulating data science problems, analyzing data, developing data-driven solutions, and presenting findings to inform high-level decisions in a broad range of application domains. As such, it incorporates skills from computer science, statistics, information science, mathematics, data visualization, information visualization, data sonification, data integration, graphic design, complex systems, communication and business.[12][13] Statistician Nathan Yau, drawing on Ben Fry, also links data science to human–computer interaction: users should be able to intuitively control and explore data.[14][15] In 2015, the American Statistical Association identified database management, statistics and machine learning, and distributed and parallel systems as the three emerging foundational professional communities.[16]"
+file_path = 'C:/Users/mtuma/Manifesto-Summarisation-Project/Manifestos/LabourManifesto.txt'
+
+with open(file_path, 'r', encoding='utf-8') as file:
+    file_content = file.read()
+
+sequence = file_content
 
 inputs = tokenizer.encode("summarize: " + sequence, return_tensors='pt', max_length=512, truncation=True)
 
-outputs = model.generate(inputs, max_length=150, min_length=10, length_penalty=5., num_beams=2)
+outputs = model.generate(inputs, max_length=200, min_length=200, length_penalty=1., num_beams=2)
 
 summary = tokenizer.decode(outputs[0])
 
